d380eeaa3fcd6c038084c7d143ce40bf519e84a4
PedjaT <pedjat91@gmail.com>
2023-12-09 11:05:14 +0100

    day 9

---
+++ Day 9/Day 9.py
@@ -0,0 +1,39 @@
+from definitions import read_inputs, read_inputs2
+import math
+
+input_example, puzzle_input = read_inputs('Day 9')
+
+try:
+    input_example2 = read_inputs2('Day 9')
+except:
+    pass
+
+
+def soolution(example):
+    s = example.split("\n")
+    predictions1 = []
+    predictions2 = []
+    for l in s:
+        l = [int(x) for x in l.split(' ')]
+        ldiffs = [l]
+        while not all(v == 0 for v in ldiffs[-1]):
+            ldiffs.append([j - i for i, j in zip(ldiffs[-1][:-1], ldiffs[-1][1:])])
+        # part 1
+        ldiffs[-1].append(0)
+        for i in range(len(ldiffs) - 2, -1, -1):
+            ldiffs[i].append(ldiffs[i][-1] + ldiffs[i + 1][-1])
+        predictions1.append(ldiffs[0][-1])
+        # part 2
+        ldiffs[-1].insert(0, 0)
+        for i in range(len(ldiffs) - 2, -1, -1):
+            ldiffs[i].insert(0, ldiffs[i][0] - ldiffs[i + 1][0])
+        predictions2.append(ldiffs[0][0])
+
+    print('Solution for part 1 is:', sum(predictions1))
+    print('Solution for part 2 is:', sum(predictions2))
+
+
+print('\ninput_example')
+soolution(input_example)
+print('\npuzzle_input')
+soolution(puzzle_input)
