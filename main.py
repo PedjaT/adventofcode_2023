bbccbe9a6f1b665832e8f4f64eab035afe8c705e
PedjaT <pedjat91@gmail.com>
2023-12-17 15:11:58 +0100

    day 17 - to be optimized

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ for c in next(os.walk(os.path.dirname(os.path.abspath(__file__))))[1]:
 
 if __name__ == '__main__':
     for d in range(1, days + 1):
+        if d in [17]: # skip slow solutions
+            continue
         print('Day ' + str(d) + ':')
         s = 'Day ' + str(d) + '.Day ' + str(d)
         globals()[s] = __import__(s)
